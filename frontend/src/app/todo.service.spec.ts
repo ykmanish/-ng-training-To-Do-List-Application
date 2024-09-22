2694a5f10eb522ab49406497284444100c502dd0
Manish <my79220@gmail.com>
2024-09-22 23:54:53 +0530

    Added test cases for User and Todos

--- frontend/src/app/todo.service.spec.ts
+++ frontend/src/app/todo.service.spec.ts
@@ -1,16 +1,67 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TodoService],
+    });
     service = TestBed.inject(TodoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create a new todo', () => {
+    const todoData = { title: 'New Todo' };
+    service.createTodo(todoData).subscribe(response => {
+      expect(response).toEqual(todoData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/todo');
+    expect(req.request.method).toBe('POST');
+    req.flush(todoData);
+  });
+
+  it('should fetch all todos', () => {
+    service.getAllTodos().subscribe(todos => {
+      expect(todos.length).toBe(2);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/todos');
+    expect(req.request.method).toBe('GET');
+    req.flush([{ title: 'Todo 1' }, { title: 'Todo 2' }]);
+  });
+
+  it('should update a todo', () => {
+    const todoData = { title: 'Updated Todo' };
+    const todoId = '123';
+    
+    service.updateTodo(todoId, todoData).subscribe(response => {
+      expect(response).toEqual(todoData);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/todo/${todoId}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush(todoData);
+  });
+
+  it('should delete a todo by ID', () => {
+    const todoId = '123';
+
+    service.deleteTodoById(todoId).subscribe(response => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/todo/${todoId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
   });
 });
--- frontend/src/app/user.service.spec.ts
+++ frontend/src/app/user.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch all users', () => {
+    service.getAllUsers().subscribe(users => {
+      expect(users.length).toBe(3);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/users');
+    expect(req.request.method).toBe('GET');
+    req.flush([{ name: 'User 1' }, { name: 'User 2' }, { name: 'User 3' }]);
   });
 });
